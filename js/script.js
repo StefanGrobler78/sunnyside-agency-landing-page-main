7e5a69df9b90a03659ec0a3796c9f63fc7aa3082
Stefan Grobler <stefangrobler78@gmail.com>
2024-05-03 09:40:38 +0200

    add Html Css and Navigation

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggler = document.getElementById('toggler');
+const menu = document.querySelector('.ss__prim-navlist')
+toggler.addEventListener('click', () => {
+    menu.classList.toggle('open')
+    console.log(menu);
+})
